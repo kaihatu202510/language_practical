c905d66a47bc649954ca6c09290236eea9b7e5ac
kaihatu202510 <atsushi.satoh@rpc-ltd.co.jp>
2026-01-11 11:58:34 +0900

    [add]java_5-1..3

---
+++ Java/src/SisxthProgram.java
@@ -0,0 +1,12 @@
+package src;
+
+public class SisxthProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 2;
+		int num2 = 10;
+		System.out.println(num1 + num2);		
+	}
+
+}
